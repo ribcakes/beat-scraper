65db612a1dce740767a2f7cbdf7be2e9d5581d12
Ribcakes <ribcakes@gmail.com>
2018-09-16 00:59:44 -0700

    Make the `slashstat` field an Object
    This field was previously a `Map<String, Long>`, however, a song was
    encountered that broke this:
    https://beatsaver.com/browse/detail/5865-5332 . The `slashstat` field
    for this song contained a `Long[]` instead. In order to not fail
    parsing, `Object` was chosen such that we retain the data, but do not
    make any guarantees about its shape.

--- src/main/java/org/ribcakes/beatScraper/details/model/StatDetail.java
+++ src/main/java/org/ribcakes/beatScraper/details/model/StatDetail.java
@@ -3,14 +3,12 @@ package org.ribcakes.beatScraper.details.model;
 import lombok.Builder;
 import lombok.Value;
 
-import java.util.Map;
-
 @Value
 @Builder
 public class StatDetail {
-    private long              time;
-    private Map<String, Long> slashstat;
-    private long              events;
-    private long              notes;
-    private long              obstacles;
+    private long   time;
+    private Object slashstat;
+    private long   events;
+    private long   notes;
+    private long   obstacles;
 }
