98e36855b3e38f32f5001d60495ef68205e17218
Ribcakes <ribcakes@gmail.com>
2018-09-15 20:31:21 -0700

    Add CollectionUtils and java8 time deserialization

--- src/main/java/org/ribcakes/beatScraper/BeatScraperApplication.java
+++ src/main/java/org/ribcakes/beatScraper/BeatScraperApplication.java
@@ -1,6 +1,7 @@
 package org.ribcakes.beatScraper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,10 @@ public class BeatScraperApplication {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        return mapper;
     }
 
     public static void main(String[] args) {
