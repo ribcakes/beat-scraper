d2325af0b474c9ab13fed3ec2944ec5b430fda8b
Ribcakes <ribcakes@gmail.com>
2019-05-27 15:49:15 -0700

    Update models to handle unknown properties
    Add `@JsonAnySetter` and `@JsonAnyGetter` annotations to handle new
    properties being added to the model. These annotations will prevent
    the application from breaking when a new field is introduced.

--- src/main/java/org/ribcakes/beatScraper/details/model/DifficultyDetail.java
+++ src/main/java/org/ribcakes/beatScraper/details/model/DifficultyDetail.java
@@ -1,14 +1,28 @@
 package org.ribcakes.beatScraper.details.model;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Value;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Value
 @Builder
 public class DifficultyDetail {
-    private String     difficulty;
-    private long       rank;
-    private String     audioPath;
-    private String     jsonPath;
-    private StatDetail stats;
+    private String              difficulty;
+    private long                rank;
+    private String              audioPath;
+    private String              jsonPath;
+    private StatDetail          stats;
+
+    @Getter(onMethod = @__(@JsonAnyGetter))
+    private Map<String, Object> properties = new HashMap<>();
+
+    @JsonAnySetter
+    public void add(String key, Object value) {
+        properties.put(key, value);
+    }
 }
--- src/main/java/org/ribcakes/beatScraper/details/model/SongDetail.java
+++ src/main/java/org/ribcakes/beatScraper/details/model/SongDetail.java
@@ -1,16 +1,24 @@
 package org.ribcakes.beatScraper.details.model;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Value;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Value
 @Builder
 public class SongDetail {
-    private long   id;
-    private String key;
-    private String name;
+    // Required Fields
+    private long          id;
+    private String        key;
+    private String        name;
+    private String        downloadUrl;
+    private CreatedDetail createdAt;
+
     private String description;
     private String uploader;
     private long   uploaderId;
@@ -24,14 +32,20 @@ public class SongDetail {
     private long   upVotesTotal;
     private long   downVotes;
     private long   downVotesTotal;
+    private long   rating;
     private String version;
     private String linkUrl;
-    private String downloadUrl;
     private String coverUrl;
     private String hashMd5;
     private String hashSha1;
 
     private Map<String, DifficultyDetail> difficulties;
 
-    private CreatedDetail createdAt;
+    @Getter(onMethod = @__(@JsonAnyGetter))
+    private Map<String, Object> properties = new HashMap<>();
+
+    @JsonAnySetter
+    public void add(String key, Object value) {
+        properties.put(key, value);
+    }
 }
